cbc345d61e7785735f23eb510a14ad4e9a9ee0e9
AKSHAY KUMAR VERMA <akshaykumarnitj@gmail.com>
2021-04-18 14:43:26 +0530

    login

---
+++ public/js/script.js
@@ -0,0 +1,16 @@
+$(()=>{
+    $('#login').click(()=>{
+        console.log('btn clicked')
+
+        $('#content').empty()
+        $('#footer_box').empty()
+
+
+
+        $('#content').load('html/login.html')
+    })
+
+
+
+})
+console.log('js connected')
