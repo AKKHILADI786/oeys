8becf51309f429c6c8deb454d84bb965cbc0ebaf
AKSHAY KUMAR VERMA <akshaykumarnitj@gmail.com>
2021-04-22 22:22:34 +0530

    herolu port and databse

--- database/models.js
+++ database/models.js
@@ -2,11 +2,27 @@ const Sequelize=require('sequelize')
 
 const data=Sequelize.DataTypes
 
+// const db;
+// if(process.env.DATABASE_URL){
+//     db=new Sequelize(process.env.DATABASE_URL)
+//   }
+//   else{
+//     db=new Sequelize({
+//       dialec:'sqlite',
+//       storage:__dirname+'/database.db'
+//     })
+//   }
+  
 
 const db=new Sequelize({
     dialect:'sqlite',
     storage:__dirname+'/database.db'
 })
+
+if(process.env.DATABASE_URL){
+    db=new Sequelize(process.env.DATABASE_URL)
+  }
+
 
 const ID={
     type:data.INTEGER,
